c657202c676258bfa46d54f8007208a3a6830017
Petr Václavek <vaclavek@educhem.cz>
2021-10-07 07:23:35 +0200

    Update genpassword.py

--- genpassword.py
+++ genpassword.py
@@ -1,9 +1,23 @@
 import random
 import string
-def genpass(l):
+from tabulate import tabulate
+def genpass(l : int):
     return "".join(random.choices(string.ascii_letters+string.digits+"!._-",k=l))
+def writepasswords(passwords : list, password_file_path : str):
+    password_file = open(password_file_path, 'wt')
+    password_file.write(tabulate(passwords, headers=["ID", "Heslo"], numalign='center', tablefmt='simple'))
+    password_file.close()
 def main():
     #TO DO Cyklus, který vygeneruje libovolný počet hesel, která budou libovolně dlouhá
-    print("")
+    pocet_hesel = int(input('Počet hesel: '))
+    delka_hesel = int(input('Délka hesel: '))
+    hesla = []
+    for i in range(pocet_hesel):
+        hesla.append([
+            i+1,
+            genpass(delka_hesel)
+        ])
+    print(tabulate(hesla, headers=["ID", "Heslo"], numalign='center', tablefmt='grid'))
+    writepasswords(hesla, './hesla.txt')
 if(__name__ == "__main__"):
     main()
